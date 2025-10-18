17be5ee93520c0ef970025abd6bba4a8b1ec273c
Yoel Gijon <yoel.canul.yacg@gmail.com>
2025-10-18 18:12:40 -0500

    feat(Footer): añade componente de pie de página con estilos y enlace a GitHub

---
+++ insert-text/src/components/3-organisms/Footer/index.tsx
@@ -0,0 +1,22 @@
+// components/3-organisms/Footer/index.tsx
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <p>
+        © {currentYear} Insert-Text | Hecho por 
+        <a 
+          href="https://github.com/JoelynCapyrby777" // <-- ¡Cambia esto por tu GitHub!
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className={styles.link}
+        >
+          Yoel 😸
+        </a>
+      </p>
+    </footer>
+  );
+}
