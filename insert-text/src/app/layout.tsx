2548e39981b608f1cf413df852f74f61c825e20f
Yoel Gijon <yoel.canul.yacg@gmail.com>
2025-10-18 18:28:10 -0500

    feat(Layout): actualiza el título de la aplicación y mejora el footer con formato

--- insert-text/src/app/layout.tsx
+++ insert-text/src/app/layout.tsx
@@ -3,11 +3,12 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Footer from '@/components/3-organisms/Footer'; // 1. Importa el Footer
 import './globals.css';
+import Header from '@/components/3-organisms/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Insert Text App',
+  title: '[Insert Text] App',
   description: 'Pega texto sin formato al instante',
 };
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={inter.className}>
+        <Header/>
         {children} {/* Aquí se renderiza tu página (page.tsx) */}
         <Footer /> {/* 2. Añade el Footer justo después */}
       </body>
