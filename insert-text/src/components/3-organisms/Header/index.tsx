cbb429fdfbdb5d3f248cb6343a4945efbfcd6867
Yoel Gijon <yoel.canul.yacg@gmail.com>
2025-10-18 18:28:03 -0500

    feat(Header): añade componente Header con estilos y enlaces de navegación

---
+++ insert-text/src/components/3-organisms/Header/index.tsx
@@ -0,0 +1,18 @@
+// components/3-organisms/Header/index.tsx
+import Link from 'next/link';
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <Link href="/" className={styles.logo}>
+          [Insert-Text]
+        </Link>
+        <Link href="/aviso-de-privacidad" className={styles.navLink}>
+          Aviso de Privacidad
+        </Link>
+      </nav>
+    </header>
+  );
+}
