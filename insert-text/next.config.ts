38be7770b0dc7238201475fb82ecd5bff48feae5
Yoel Gijon <yoel.canul.yacg@gmail.com>
2025-10-23 17:58:45 -0500

    feat: simplifica la configuración de Next.js y mejora la estructura del archivo de configuración

--- insert-text/next.config.ts
+++ insert-text/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {
+    useDeploymentId: true,
+    useDeploymentIdServerActions: true,
+  },
 };
 
-export default nextConfig;
+  export default nextConfig;
